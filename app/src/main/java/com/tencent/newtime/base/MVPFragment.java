ac4a2fc1cd6ccb75bd7c53b6b91c9e683ab72784
songchenguang <songchenguang1992@gmail.com>
2016-07-12 12:52:31 +0800

    增加界面

--- app/src/main/java/com/tencent/newtime/base/MVPFragment.java
+++
@@ -1,8 +0,0 @@
-package com.tencent.newtime.base;
-
-/**
- * Created by Liujilong on 16/1/24.
- * liujilong.me@gmail.com
- */
-public abstract class MVPFragment<View, Presenter extends BasePresenter<View>> extends BaseFragment{
-}
